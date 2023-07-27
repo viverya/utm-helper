f989e7b7d5b6ab0a8eaa3ae74a598e28cd43ac92
Anya Quote <anya.quote@gmail.com>
2023-07-27 08:14:22 +0700

    docs: write some documentation for test and example

--- example/utm_helper_example.dart
+++ example/utm_helper_example.dart
@@ -3,7 +3,7 @@ import 'package:utm_helper/utm_helper.dart';
 int main() {
   // Full valid query string
   UtmData.fromQueryString(
-      'utm_source=bangbu&utm_medium=cpc&utm_campaign=summer&utm_term=beach%20vacation&utm_content=ad1&utm_id=anyaquote');
+      'utm_source=viverya&utm_medium=cpc&utm_campaign=summer&utm_term=beach%20vacation&utm_content=ad1&utm_id=anyaquote');
 
   // Query string with some data
   UtmData.fromQueryString('utm_source=bangbu&utm_medium=cpc');
--- test/utm_helper_test.dart
+++ test/utm_helper_test.dart
@@ -2,7 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:utm_helper/utm_helper.dart';
 
 void main() {
+  /// Test UtmData.fromQueryString() method
+  /// This method is used to generate an UtmData object from query string
   group('utm data factory: decoded query string', () {
+    /// Test with full query string
     test('should return full UtmData object', () {
       final utmData = UtmData.fromQueryString(
           'utm_source=google&utm_medium=cpc&utm_campaign=summer&utm_term=beach%20vacation&utm_content=ad1&utm_id=1234567890');
@@ -14,6 +17,7 @@ void main() {
       expect(utmData.id, '1234567890');
     });
 
+    /// Test with invalid query string
     test('should have null value if query string is not valid', () {
       final utmData = UtmData.fromQueryString('some-invalid-query-string');
       expect(utmData.source, null);
@@ -24,6 +28,7 @@ void main() {
       expect(utmData.id, null);
     });
 
+    /// Test with query string that has some null value
     test('should have some null value that query string does not have', () {
       final utmData = UtmData.fromQueryString('utm_source=bangbu&utm_id=anya');
       expect(utmData.source, 'bangbu');
