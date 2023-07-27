6a3d0c5497393bbb0df5cd13406c3e6c85088551
Anya Quote <anya.quote@gmail.com>
2023-07-27 08:11:40 +0700

    docs(utm_data): write document for properties

--- lib/src/utm_data.dart
+++ lib/src/utm_data.dart
@@ -1,12 +1,37 @@
 /// Class contains UTM data
 class UtmData {
+  /// utm_source
+  /// Identifies which site sent the traffic, and is a required parameter.
+  /// Example: google, newsletter, billboard.
   final String? source;
+
+  /// utm_medium
+  /// Identifies what type of link was used, such as cost per click or email.
+  /// Example: cpc, banner, email newsletter.
   final String? medium;
+
+  /// utm_campaign
+  /// Identifies a specific product promotion or strategic campaign.
+  /// Example: utm_campaign=spring_sale.
   final String? campaign;
+
+  /// utm_term
+  /// Identifies search terms.
+  /// Example: running+shoes.
   final String? term;
+
+  /// utm_content
+  /// Identifies what specifically was clicked to bring the user to the site, such as a banner ad or a text link.
+  /// It is often used for A/B testing and content-targeted ads.
+  /// Example: logolink or textlink.
   final String? content;
+
+  /// utm_id
+  /// Identifies a specific visitor, customer, or client.
+  /// Example: 1234567890.
   final String? id;
 
+  /// Constructor
   UtmData({
     this.source,
     this.medium,
@@ -30,6 +55,7 @@ class UtmData {
     );
   }
 
+  /// Generate a string from UtmData object
   @override
   String toString() {
     return 'UtmData { '
